8e9bc7a11a68f809996d9794bb2c488d489143d2
pierrebgs <pbergeras01@gmail.com>
2023-01-13 11:46:09 +0100

    Initial commit

---
+++ level1/main.py
@@ -0,0 +1,37 @@
+import json
+from datetime import datetime, timedelta
+
+class Car:
+    def __init__(self, id, price_per_day, price_per_km):
+        self.id = id
+        self.price_per_day = price_per_day
+        self.price_per_km = price_per_km
+
+    def rental_price(self, start_date, end_date, distance):
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        days = (end_date - start_date).days + 1
+        time_component = days * self.price_per_day
+        distance_component = distance * self.price_per_km
+        return time_component + distance_component
+
+# load data from input.json
+with open("data/input.json") as json_file:
+    data = json.load(json_file)
+
+# create a map of car id to car object 
+car_map = {}
+for car_data in data["cars"]:
+    car = Car(car_data["id"], car_data["price_per_day"], car_data["price_per_km"])
+    car_map[car.id] = car
+    
+result = {}
+result["rentals"] = []
+for rental_data in data["rentals"]:
+    car = car_map[rental_data["car_id"]]
+    rental_price = car.rental_price(rental_data["start_date"], rental_data["end_date"], rental_data["distance"])
+    result["rentals"].append({"id": rental_data["id"], "price": rental_price})
+
+# save data into output.json
+with open("data/output.json", "w") as json_file:
+    json.dump(result, json_file)
