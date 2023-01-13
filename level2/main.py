b95cfdb68b14f969deaeff2b28e73120a59a8271
pierrebgs <pbergeras01@gmail.com>
2023-01-13 15:31:15 +0100

    Level 1 and level2

--- level2/main.py
+++ level2/main.py
@@ -11,9 +11,21 @@ class Car:
         start_date = datetime.strptime(start_date, '%Y-%m-%d')
         end_date = datetime.strptime(end_date, '%Y-%m-%d')
         days = (end_date - start_date).days + 1
-        time_component = days * self.price_per_day
+        time_component = 0
+        price_per_day = self.price_per_day
+        for i in range(days):
+            if i == 0:
+                time_component += self.price_per_day
+            elif 1 <= i < 4:
+                time_component += self.price_per_day * 0.9
+            elif 4 <= i < 10:
+                time_component += self.price_per_day * 0.7
+            else:
+                time_component += self.price_per_day * 0.5
         distance_component = distance * self.price_per_km
-        return time_component + distance_component
+        return int(time_component + distance_component)
+
+
 
 # load data from input.json
 with open("data/input.json") as json_file:
@@ -32,6 +44,6 @@ for rental_data in data["rentals"]:
     rental_price = car.rental_price(rental_data["start_date"], rental_data["end_date"], rental_data["distance"])
     result["rentals"].append({"id": rental_data["id"], "price": rental_price})
 
-# save data into output.json
+# save data to output.json
 with open("data/output.json", "w") as json_file:
     json.dump(result, json_file, indent = 2)
